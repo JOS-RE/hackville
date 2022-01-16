753e5b77312dfbc039dc01cc692fb3c3da16d66a
Anurag30112003 <anuragsharma011011@gmail.com>
2022-01-16 16:05:55 +0530

    Revert "work-pls"
    This reverts commit 1905c4c6f636a5b497d48e6da3deed306af4802d.

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from website import app
-
+from website import create_app
+from flask import Flask
 if __name__ == "__main__":
+    app = create_app()
+    # app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
     app.run(debug=True)
